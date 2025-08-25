3cf426e24fda7e540a48f1d52572884de6c8cb61
alperenynk <alperenyanik@hotmail.com>
2025-08-25 09:48:23 +0300

    feat: lesson 5

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,30 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import { StyleSheet, useColorScheme } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { Colors } from "../constants/Colors";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light; // null veya undefined olursa eğer Colors.light yani sağ taraf seçilir. ?? bu anlama geliyor
+
   return (
-    <Stack screenOptions={{
-        headerStyle: { backgroundColor: "#ddd" },
-        headerTintColor: "#333",
-    }}>
+    <>
+      <StatusBar value="auto" />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
+        }}
+      >
         <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="about" options={{ title: "About" }} />
-        <Stack.Screen name="contact" options={{ title: "Contact", headerShown: false }} />
-    </Stack>
-  )
-}
+        <Stack.Screen name="contact" options={{ title: "Contact" }} />
+      </Stack>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- app/about.jsx
+++ app/about.jsx
@@ -1,32 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, useColorScheme } from "react-native";
+import React from "react";
 import { Link } from "expo-router";
+import { Colors } from "../constants/Colors";
 
 const About = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>About Page</Text>
-      <Link href="/" style={styles.link}>Home Page</Link>
-    </View>
-  )
-}
 
-export default About
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light; // null veya undefined olursa eğer Colors.light yani sağ taraf seçilir. ?? bu anlama geliyor
+
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <Text style={styles.title}>About Page</Text>
+      <Link href="/" style={styles.link}>
+        Home Page
+      </Link>
+    </View>
+  );
+};
+
+export default About;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-    title: {
-        fontWeight: "bold",
-        fontSize: 18,
-    },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
 
-    link: {
-        marginVertical: 10,
-        borderBottomWidth: 1,
-    },
-})
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1,
+  },
+});
