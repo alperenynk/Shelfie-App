b5c3edfa15247d384d394ad27761293935a18220
alperenynk <alperenyanik@hotmail.com>
2025-08-29 00:55:45 +0300

    feat: lesson 8

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,43 +1,51 @@
-import { StyleSheet } from 'react-native'
-import React from 'react'
-import { Link } from 'expo-router'
+import { StyleSheet, Text } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 // Themed Components
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
+import ThemedButton from "../../components/ThemedButton";
 import Spacer from "../../components/Spacer";
 
 const Login = () => {
+  const handleSubmit = () => {
+    console.log("login form submitted");
+  };
+
   return (
     <ThemedView style={styles.container}>
+      <Spacer />
+      <ThemedText title={true} style={styles.title}>
+        Login to Your Account
+      </ThemedText>
 
-        <Spacer />
-        <ThemedText title={true} style={styles.title}>
-            Login to Your Account
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Login</Text>
+      </ThemedButton>
+
+      <Spacer height={100} />
+      <Link href="/register">
+        <ThemedText style={{ textAlign: "center" }}>
+          Register Instead
         </ThemedText>
-
-        <Spacer height={100} />
-        <Link href="/register">
-            <ThemedText style={{ textAlign: "center" }}>
-                Register Instead
-            </ThemedText>
-        </Link>
-
+      </Link>
     </ThemedView>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    title: {
-        textAlign: "center",
-        fontSize: 18,
-        marginBottom: 30,
-    },
-})
+  title: {
+    textAlign: "center",
+    fontSize: 18,
+    marginBottom: 30,
+  },
+});
