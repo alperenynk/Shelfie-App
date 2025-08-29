e8553f2dbb2fbf7aec604fe443185b6f55e25181
alperenynk <alperenyanik@hotmail.com>
2025-08-29 09:42:05 +0300

    feat: lesson 9

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,32 +1,28 @@
-import { StyleSheet, useColorScheme } from "react-native";
-import React from "react";
 import { Stack } from "expo-router";
 import { Colors } from "../constants/Colors";
+import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-const RootLayout = () => {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const theme = Colors[colorScheme] ?? Colors.light; // null veya undefined olursa eğer Colors.light yani sağ taraf seçilir. ?? bu anlama geliyor
+  const theme = Colors[colorScheme] ?? Colors.light;
 
   return (
     <>
       <StatusBar value="auto" />
-      <Stack
+      <Stack initialRouteName="index"
         screenOptions={{
           headerStyle: { backgroundColor: theme.navBackground },
           headerTintColor: theme.title,
         }}
       >
-
+        {/* Groups */}
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
 
+        {/* Individual Screens */}
         <Stack.Screen name="index" options={{ title: "Home" }} />
-
       </Stack>
     </>
   );
-};
-
-export default RootLayout;
-
-const styles = StyleSheet.create({});
+}
