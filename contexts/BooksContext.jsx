8b14cedef95be82109cc700725abc744e25750c1
alperenynk <alperenyanik@hotmail.com>
2025-09-03 11:02:50 +0300

    feat: lesson 23

--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react";
+import { databases } from "../lib/appwrite"
+import { ID, Permission, Role } from "react-native-appwrite";
+import { useUser } from "../hooks/useUser"
 
 const DATABASE_ID = "68b6d9980002ad71c3b0"
 const COLLECTION_ID = "books" 
@@ -7,6 +10,7 @@ export const BooksContext = createContext()
 
 export function BooksProvider({ children }) {
     const [books, setBooks] = useState([])
+    const { user } = useUser()
 
     async function fetchBooks() {
         try {
@@ -26,7 +30,17 @@ export function BooksProvider({ children }) {
 
     async function createBook(data) {
         try {
-
+            const newBook = await databases.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {...data, userId: user.$id},
+                [
+                    Permission.read(Role.user(user.$id)),
+                    Permission.update(Role.user(user.$id)),
+                    Permission.delete(Role.user(user.$id)),
+                ]
+            )
         } catch (error) {
             console.error(error.message)
         }
@@ -41,7 +55,7 @@ export function BooksProvider({ children }) {
     }
 
     return (
-        <BooksContext.Provider value={{ books, fetchBooks, fetchBookById, createBooks, deleteBook }}>
+        <BooksContext.Provider value={{ books, fetchBooks, fetchBookById, createBook, deleteBook }}>
             {children}
         </BooksContext.Provider>
     )
